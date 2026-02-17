c057cdc427d9cc0487ef61fbc1dc383f2b1abf1c
MegaFusion13 <megafusion309@gmail.com>
2026-02-17 14:19:00 +0100

    Update files

---
+++ src/Liste.h
@@ -0,0 +1,116 @@
+#include "ProcStruct.h"
+
+typedef struct celluleListe {
+    /**
+     * Structure d'une liste chainée
+     * @var proc : processus stocké 
+     * @var suivant : cellule suivante
+     */
+
+    Processus proc;
+    struct celluleListe* suivant;
+}*Liste;
+
+int estVideListe(Liste l){
+    /**
+     * Verifie si la liste est vide, renvoie Vrai si vide ,Faux sinon
+     * @param l : La Liste à vérifier
+     * @return 1 ou 0
+     */
+    return l == NULL;
+}
+
+Liste initListe(){
+    /**
+     * Initialise une liste vide
+     * @return une liste vide (NULL)
+     */
+    return NULL;
+}
+
+Processus procListe(Liste l){
+    /**
+     * Renvoie le processus de la cellule en paramètre
+     * @param l : pointeur sur la cellule
+     * @return le processus courant
+     */
+    return l->proc;
+}
+
+Liste suivantListe(Liste l){
+    /**
+     * Renvoie la cellule suivante de la cellule en paramètre
+     * @param l : pointeur sur la cellule
+     * @return la cellule suivante
+     */
+    return l->suivant;
+}
+
+void supTeteListe(Liste *l){
+    /**
+     * Supprime la tete de la liste, passage par adresse
+     * @param l : adresse de la liste de processus
+     */
+    Liste ll;
+    if (estVideListe(*l)){
+        printf("la liste est deja vide\n");
+        exit(0);
+    }
+    ll = *l;
+    *l = suivantListe(*l);
+    free(ll);
+}
+
+Liste inserTeteListe(Liste l, Processus p){
+    /**
+     * Ajoute une cellule à la tête de la liste donnée
+     * @param l : pointeur sur la liste
+     * @param p : le processus contenu dans la cellule
+     * @return La liste mis à jour
+     */
+    Liste tmp = (Liste)malloc (sizeof(struct celluleListe));
+    if (tmp == NULL){
+        printf("Erreur d'allocation liste\n");
+        exit(1);
+    }
+    tmp->proc = p;
+    tmp->suivant = l;
+    l = tmp;
+    return l;
+}
+Liste inserProcListe(Liste l,Processus p){
+    /**
+     * Insertition en queue d'une liste
+     * @param l : la Liste de processus
+     * @param p : le processus à ajouter
+     * @return La liste mis à jour
+     */
+    Liste ll = l;
+    if (estVideListe(l)){
+        return inserTeteListe(l,p);
+    }
+    while (!estVideListe(ll->suivant)){
+        ll = l->suivant;
+    }
+    ll->suivant = inserTeteListe(ll->suivant,p);
+    return l;
+}
+
+int supElement(Liste l,int pid){
+    /**
+     * Supprime une cellule dans une liste de processus
+     * @param l     : La liste de processus
+     * @param pid   : numéro du processus
+     * @return Une verification : 1 si le processus est trouvé, 0 sinon
+     */
+    if (estVideListe(l)){
+        return 0;
+    }
+    if (l->proc.pid == pid){
+        supTeteListe(&l);
+        return 1;
+    }
+    else{
+        return supElement(l->suivant,pid);
+    }
+}
--- src/ProcStruct.h
+++ src/ProcStruct.h
@@ -3,26 +3,56 @@
 #include <stdlib.h>
 
 typedef struct cellule{
+    /**
+     * Structure d'un cycle contenant une liste de durée
+     * @var duree   : temps
+     * @var suivant : cellule suivante
+     */
     int duree;
     struct cellule* suivant;
 }*Cycle;
 
 Cycle initCycle(){
+    /**
+     * Initialise un Cycle
+     * @return un Cycle vide
+     */
     return NULL;
 }
 int estVideCycle(Cycle c){
+    /**
+     * Verifie si un cycle est vide
+     * @param c : le cycle à verifier
+     * @return  : 1 si vide, 0 sinon 
+     */
     return (c == NULL);
 }
 int get_DureeCycle(Cycle c){
+    /**
+     * Renvoie la durée stockée dans une cellule
+     * @param c : pointeur vers la cellule
+     * @return la durée
+     */
     if (estVideCycle(c)){
         return 0;
     }
     return c->duree;
 }
 Cycle suivantCycle(Cycle c){
+    /**
+     * Renvoie la cellule suivante
+     * @param c : la cellule courante
+     * @return la cellule suivante
+     */
     return c->suivant;
 }
 Cycle inserTeteDureeCycle(Cycle c, int temps){
+    /**
+     * Insertion en tête d'un cycle
+     * @param c     : pointeur sur la cellule du cycle
+     * @param temps : la durée contenu dans la cellule
+     * @return Le cycle mis à jour
+     */
     Cycle tmp = malloc (sizeof(struct cellule));
     if (tmp == NULL){
         printf("Erreur d'allocation\n");
@@ -34,6 +64,12 @@ Cycle inserTeteDureeCycle(Cycle c, int temps){
     return c;
 }
 Cycle inserDureeCycle(Cycle c,int temps){
+    /**
+     * Insertion en queue du cycle donné
+     * @param c     : pointeur sur la cellule du cycle
+     * @param temps : la durée contenu dans la cellule
+     * @return Le cycle mis à jour
+     */
     Cycle cc = c;
     if (estVideCycle(c)){
         return inserTeteDureeCycle(c,temps);
@@ -46,6 +82,10 @@ Cycle inserDureeCycle(Cycle c,int temps){
 }
 
 void supTeteCycle(Cycle *c){
+    /**
+     * Supprime la tête du cycle donnée
+     * @param c : adresse vers cycle
+     */
     Cycle cc;
     if (estVideCycle(*c)){
         printf("le cycle est deja vide\n");
@@ -57,9 +97,18 @@ void supTeteCycle(Cycle *c){
 }
 
 typedef struct {
+    /**
+     * Structure d'un processus
+     * @var pid     : numéro du processus
+     * @var arrive  : temps d'arrivé dans le système
+     * @var ES      : Cycle entrées/sorties
+     * @var CPU     : Cycle CPU
+     * @var etat    : état actuel du processus
+     */
+    int pid;
     int arrive;
-    Cycle ES;     /* Durée E/S */
-    Cycle CPU;    /* Durée CPU */
+    Cycle ES;
+    Cycle CPU;
     int etat;   /* état du processus - 0 : non arrivé
                                      - 1 : pret
                                      - 2 : en_cours
@@ -71,23 +120,44 @@ typedef struct {
 }Processus;
 
 Cycle get_ES(Processus p){
+    /**
+     * Renvoie le cycle E/S d'un processus
+     */
     return p.ES;
 }
 Cycle get_CPU(Processus p){
+    /**
+     * Renvoie le cycle CPU d'un processus
+     */
     return p.CPU;
 }
 int non_arrive(Processus p){
+    /**
+     * Verifie si un processus n'est pas encore arrivé dans le système
+     */
     return (p.etat == 0);
 }
 int est_pret(Processus p){
+    /**
+     * Verifie si un processus est prêt
+     */
     return (p.etat == 1);
 }
 int est_en_cours(Processus p){
+    /**
+     * Verifie si un processus est en_cours
+     */
     return (p.etat == 2);
 }
 int est_bloque(Processus p){
+    /**
+     * Verifie si un processus est bloqué
+     */
     return (p.etat == 3);
 }
 int est_termine(Processus p){
+    /**
+     * Verifie si un processus est terminé
+     */
     return (p.etat == 4);
 }
